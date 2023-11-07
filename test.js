0e6562e85807e442e3671be7a46e90463bb89041
Shivam Gupta <shivamgupta@Shivams-MacBook-Air.local>
2023-11-07 15:19:34 +0530

    just normal cleanup

--- test.js
+++ test.js
@@ -1,32 +1,19 @@
-//jshint esversion:6
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const multer = require("multer");
-const bodyParser = require("body-parser");
-const path = require("path"); 
-const bcrypt = require("bcrypt")
-const morgan = require("morgan")
-const mongoose = require("mongoose");
-const db = require('./managers/models/db')
-const routesApi = require('./api');
-
-app.set('view engine', 'ejs'); // Set EJS as the default template engine
-app.set('views', path.join(__dirname, 'views')); // Set views directory
-app.use(bodyParser.json({extended: true}));
-app.use(express.static("public"));
-
-app.use(morgan(':method :url :status :user-agent - [:date[clf]] :response-time ms'));
-app.use('/api/auth/images', express.static(path.join(__dirname, 'uploads')));
-
-  
-app.use('/api', routesApi);
-console.log("here")
-
-ports = [process.env.PORT, process.env.PORT1, process.env.PORT2]
-// Loop through the ports and start the server on each port
-ports.forEach((port) => {
-    app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-    });
-});
+"axios": "^1.5.1",
+"bcrypt": "^5.1.1",
+"body-parser": "^1.20.2",
+"cookie-parser": "^1.4.6",
+"dotenv": "^16.3.1",
+"ejs": "^3.1.9",
+"express": "^4.18.2",
+"fcm-node": "^1.3.0",
+"html-minifier": "^4.0.0",
+"html-pdf": "^1.5.0",
+"jsonwebtoken": "^9.0.2",
+"moment": "^2.29.4",
+"mongoose": "^7.6.1",
+"morgan": "^1.10.0",
+"multer": "^1.4.5-lts.1",
+"nodemailer": "^6.9.6",
+"nodemon": "^3.0.1",
+"uuid": "^9.0.1",
+"validate.js": "^0.13.1"
